90eabdbb1e071756f157c2b38b738e22a5df9e1a
Bryan Aritcheta <bryanaritcheta@gmail.com>
2022-02-21 19:36:28 +0800

    Implementation of payload

--- redux/cakeshop/src/App.js
+++ redux/cakeshop/src/App.js
@@ -2,15 +2,17 @@ import React from 'react'
 import { Provider } from 'react-redux';
 import store from './redux/store'
 import './App.css';
-import CakeContainer from './components/CakeContainer';
+// import CakeContainer from './components/CakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import CookieContainer from './components/CookieContainer';
+import NewCakeContainer from './components/NewCakeContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <CakeContainer />
+        {/* <CakeContainer /> */}
+        <NewCakeContainer />
         <IceCreamContainer />
         <CookieContainer />
       </div>
